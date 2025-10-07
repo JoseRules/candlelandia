265627c58d611390f4bca3243d7de4499d2c6147
Jose Flores <jose.fgonzalez@alumnos.udg.mx>
2025-10-07 08:41:38 -0600

    style changes

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,5 @@
+import CandleIcon from "./CandleIcon";
+import ArrowIcon from "./ArrowIcon";
+import CartIcon from "./CartIcon";
+
+export { CandleIcon, ArrowIcon, CartIcon };
---
+++ src/utils/products.ts
@@ -0,0 +1,56 @@
+export const products = [
+  {
+    id: 1,
+    name: "Lavender Dreams",
+    description: "A soothing blend of lavender and vanilla that creates the perfect atmosphere for relaxation and sleep.",
+    price: 24.99,
+    rating: 4.8,
+    image: "https://velasmariat.com/cdn/shop/files/DSC_1361_ed4996fd-7d1e-4034-9bf8-960a16c636be_1024x1024@2x.jpg?v=1710018042",
+    reviews: 127
+  },
+  {
+    id: 2,
+    name: "Rose Garden",
+    description: "Elegant rose petals combined with subtle jasmine notes for a romantic and luxurious ambiance.",
+    price: 29.99,
+    rating: 4.9,
+    image: "https://velasmariat.com/cdn/shop/articles/DSC_3807_300x300.jpg?v=1633559114",
+    reviews: 89
+  },
+  {
+    id: 3,
+    name: "Ocean Breeze",
+    description: "Fresh sea salt and ocean air scents that bring the calming essence of the coast to your home.",
+    price: 22.99,
+    rating: 4.7,
+    image: "https://velasart.com/wp-content/uploads/2023/02/many-candles-burning-at-night-many-candle-flames-2022-10-04-18-19-37-utc.jpg",
+    reviews: 156
+  },
+  {
+    id: 4,
+    name: "Vanilla Cream",
+    description: "Rich, creamy vanilla with hints of caramel - perfect for creating a warm, cozy atmosphere.",
+    price: 19.99,
+    rating: 4.6,
+    image: "https://velasart.com/wp-content/uploads/2023/03/Tea-Descripcion-1.png",
+    reviews: 203
+  },
+  {
+    id: 5,
+    name: "Citrus Burst",
+    description: "Energizing blend of lemon, orange, and grapefruit that brightens any space with fresh vibrancy.",
+    price: 21.99,
+    rating: 4.5,
+    image: "https://velasart.com/wp-content/uploads/2023/02/burning-advent-candles-2022-11-10-18-01-32-utc.jpg",
+    reviews: 94
+  },
+  {
+    id: 6,
+    name: "Sandalwood Serenity",
+    description: "Exotic sandalwood and patchouli create a meditative atmosphere perfect for mindfulness and focus.",
+    price: 27.99,
+    rating: 4.8,
+    image: "https://velasart.com/wp-content/uploads/2023/02/colorful-stick-candles-arranged-on-shelves-and-sor-2022-05-11-05-37-51-utc.jpg",
+    reviews: 78
+  }
+];
